625c0ca037abc8141d5167eb114b8de86a8ea9e8
TFF <damo@WALKER.station>
2026-05-13 17:13:38 +0100

    disable supabase realtime to fix ws error

--- app/supabase.server.js
+++ app/supabase.server.js
@@ -3,4 +3,11 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: {
+    enabled: false,
+  },
+  global: {
+    headers: {},
+  },
+});
